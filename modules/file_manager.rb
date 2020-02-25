5ab8485f3a3fefe9fcc07ebfc42b8d57011270b3
galuropek <galuropek@gmail.com>
2020-02-25 21:08:09 +0300

    open_file and read_file methods for management

--- modules/file_manager.rb
+++ modules/file_manager.rb
@@ -2,6 +2,8 @@ require 'csv'
 
 module FileManager
 
+  # @param [String] path - path to file
+  # @param [String] col_sep in csv file
   def get_table_from_csv(path, col_sep = nil)
     CSV.parse(File.read(path), headers: true, col_sep: col_sep || "\t")
   end
@@ -15,12 +17,24 @@ module FileManager
     end
   end
 
+  # @param [String] file_path - path to file
+  # @param [String] text - text to output file
   def write_to_file(file_path, text, mode = "w")
     out_path = file_path.gsub(/\.\w+$/, '_result.txt')
-    file = File.open(out_path, mode)
+    file = File.open(file_path, mode)
     file.write(text)
-    file.close
+    close_file(file)
     puts "See result in:
 #{out_path}"
+  end
+
+  # @param [String] path - path to file
+  def open_file(path, mode = "r")
+    File.open(path, mode)
+  end
+
+  # @param [File] file
+  def close_file(file)
+    file.close
   end
 end
