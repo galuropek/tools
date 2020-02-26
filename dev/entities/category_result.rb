b32999fc2407a3b94193093ebd1b21b598457bef
galuropek <galuropek@gmail.com>
2020-02-26 22:48:30 +0300

    Added breadcrumb parser

--- dev/entities/category_result.rb
+++ dev/entities/category_result.rb
@@ -1,12 +1,14 @@
+require 'colorize'
+
 class CategoryResult < Result
-  attr_accessor :breadcrumb, :url, :retailer, :level, :br_sep
+  attr_accessor :breadcrumb, :url, :retailer, :level, :br_sep, :parsed_breadcrumb
 
   def initialize
     @br_sep = '***'
   end
 
   def calc_level
-    @level = @breadcrumb.split(@br_sep).count
+    @level = do_parse_br.count
   end
 
   def to_string
@@ -15,5 +17,14 @@ class CategoryResult < Result
 
   def get(key)
     instance_variable_get("@#{key}")
+  end
+
+  def parse_breadcrumb
+    @parsed_breadcrumb = do_parse_br
+  end
+
+  def do_parse_br
+    puts "Used '#{@br_sep}' br_sep for split breadcrumb!!! Please check your br_sep from input file. It affects 'breadcrumb_count' and splitted breadcrumb for comparing!!!".red
+    @breadcrumb.split(@br_sep).map(&:strip)
   end
 end
